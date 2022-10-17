fc56ac006a306acd9ecede95c9db8787048737f7
Jason Goemaat <jason.goemaat@gmail.com>
2022-10-16 19:42:53 -0500

    Added sample in bitburner-scripts

---
+++ bitburner-scripts/external.js
@@ -0,0 +1,55 @@
+
+let externalWindow = null
+
+export function openWindow(url) {
+  if (!externalWindow || !externalWindow.self) {
+    externalWindow = window.open(url, '_blank',
+    `popup=true,width=1920,height=1000,left=0,top=0`);
+    let ew = eval('window.ew = window.ew || []')
+    ew.push(externalWindow)
+    console.log('added external window:', externalWindow)
+    console.log(`available as ew[${ew.length - 1}]`)
+  } else {
+    console.log('existing external window:', externalWindow)
+  }
+}
+
+// TODO: not implemented
+export function logActions(id, actions) {
+  if (externalWindow) {
+    // console.log('posint message:')
+    externalWindow.postMessage({
+      command: 'logActions',
+      id,
+      actions
+    }, '*')
+    return true
+  }
+  return false
+}
+
+export function log(id, lines) {
+  if (externalWindow) {
+    // console.log('posint message:')
+    externalWindow.postMessage({
+      command: 'log',
+      id,
+      lines
+    }, '*')
+    return true
+  }
+  return false
+}
+
+// TODO: not implemented
+export function table(id, data) {
+  if (externalWindow) {
+    externalWindow.postMessage({
+      command: 'table',
+      id,
+      data
+    }, '*')
+    return true
+  }
+  return false
+}
